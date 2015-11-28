dbf15ba670136e4d3e764ea6060c8bd8ffa55ed7
Gustavo Sobral <ghsobral@gmail.com>
2015-11-28 16:23:17 -0300

    Adds initial files

---
+++ src/main/java/ufpb/br/memorygenerator/MemoryGenerator.java
@@ -0,0 +1,8 @@
+package main.java.ufpb.br.memorygenerator;
+
+public class MemoryGenerator {
+
+    public static void main(String[] args) {
+	    System.out.println("Olá mundo");
+    }
+}
---
+++ src/test/java/ufpb/br/memorygenerator/MemoryGeneratorTest.java
@@ -0,0 +1,17 @@
+package test.java.ufpb.br.memorygenerator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Created by gustavo on 28/11/15.
+ */
+public class MemoryGeneratorTest {
+
+    @Test
+    public void fileTest(){
+
+    }
+
+}
